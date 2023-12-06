b9d4684fb59c6136a55620438fe5989407b4b1d1
illyaaaaa <ishevelov33@gmail.com>
2023-12-06 00:55:18 +0000

    updated add method in ColourTable

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+
 public class ColourTable {
     private int paletteSize;
     private int[][] palette;
+    private int coloursAdded;
 
     public ColourTable(int paletteSize) {
         if (!isValidPaletteSize(paletteSize)) {
@@ -9,22 +11,40 @@ public class ColourTable {
         }
         this.paletteSize = paletteSize;
         this.palette = new int[paletteSize][3];
+        this.coloursAdded = 0;
     }
 
     public int getPaletteSize() {
         return paletteSize;
     }
 
-    public boolean isValidPaletteSize(int size){
+    public boolean isValidPaletteSize(int size) {
         return size > 1 && (size & (size - 1)) == 0 && size < 1025;
     }
 
-    public void add(int[] rgbColour){
-        if (!isValidRGB(rgbColour)){
+    public void add(int[] rgbColour) {
+        if (!isValidRGB(rgbColour)) {
             throw new IllegalArgumentException("Invalid RGB Format. Must be between 0-255");
         }
+
+        if (isPaletteFull()) {
+            throw new IllegalArgumentException("Exceeded the capacity of the ColourTable.");
+        }
+
+        palette[coloursAdded] = Arrays.copyOf(rgbColour, 3);
+        coloursAdded++;
     }
-    public boolean isValidRGB(int[] rgbColour){
-        return Arrays.stream(rgbColour).allMatch(value -> value >= 0 && value <= 255);
+
+    private boolean isPaletteFull() {
+        return coloursAdded == paletteSize;
+    }
+
+    public boolean isValidRGB(int[] rgbColour) {
+        return Arrays.stream(rgbColour).allMatch(value -> value >= 0 && value <= 255) && rgbColour.length == 3;
+    }
+
+    public int[][] getPalette() {
+        return palette;
     }
 }
+
